0532afcf4166984b162f48b2f00efefe252dea86
Stephen Lorence <sklorence@gmail.com>
2020-12-15 20:53:14 -0600

    Updating GitHub with changes from home PC.

---
+++ Part 1/Section 09 - Modules, Packages and Namespaces/example1/main.py
@@ -0,0 +1,16 @@
+import sys
+
+print('=====================================')
+print(f'Running main.py - module name: {__name__}')
+
+import module1
+
+print('importing module1 again...')
+del globals()['module1']
+
+import module1
+
+module1.pprint_dict('main.globals', globals)
+
+
+print('=====================================')
