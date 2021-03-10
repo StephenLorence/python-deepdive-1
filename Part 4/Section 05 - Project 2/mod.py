cb6c05b90f6d11c66cd689ba646e510415b47ed2
Stephen Lorence <sklorence@gmail.com>
2021-03-09 20:51:52 -0600

    Synching from home PC.

--- Part 4/Section 05 - Project 2/mod.py
+++ Part 4/Section 05 - Project 2/mod.py
@@ -24,6 +24,21 @@ class Mod:
     def modulus(self):
         return self._modulus
     
+    def int_to_mod(self, value):
+        return Mod(value, self.modulus)
+    
+    def validate_mod_math(self, other):
+        if self.modulus != other.modulus:
+            raise ValueError('Both Mod objects must have the same modulus to support this operation.')
+            
+    def _math_prep(self, other):
+        if isinstance(other, int):
+            other = self.int_to_mod(other)
+        if isinstance(other, Mod):
+            self.validate_mod_math(other)
+            return other
+        return NotImplemented
+    
     def __eq__(self, other):
         if isinstance(other, Mod):
             return self.modulus == other.modulus and self.value == other.value
@@ -33,3 +48,25 @@ class Mod:
     
     def __hash__(self):
         return hash((self.value, self.modulus))
+    
+    def __int__(self):
+        return self.value
+    
+    def __repr__(self):
+        return f'Mod(value={self.value}, modulus={self.modulus})'
+    
+    def __add__(self, other):
+        other = self._math_prep(other)
+        return Mod(self.value + other.value, self.modulus)
+    
+    def __sub__(self, other):
+        other = self._math_prep(other)
+        return Mod(self.value - other.value, self.modulus)
+        
+    def __mul__(self, other):
+        other = self._math_prep(other)
+        return Mod(self.value * other.value, self.modulus)
+    
+    def __pow__(self, other):
+        other = self._math_prep(other)
+        return Mod(self.value ** other.value, self.modulus)
--- Part 4/Section 05 - Project 2/test_mod.py
+++ Part 4/Section 05 - Project 2/test_mod.py
@@ -2,10 +2,6 @@ import pytest
 from mod import Mod
 import os
 
-@pytest.fixture
-def create_test_mod():
-    return Mod(5, 3)
-
 @pytest.fixture(params=[(5, 3), (7, 2), (16, 5)])
 # Apparently when you parametrize a fixture, you need to pass this
 # 'request' object in as a parameter to the decorated function, and
@@ -26,9 +22,13 @@ def mod_22():
 def mod_53():
     return Mod(5, 3)
 
+@pytest.fixture
+def mod_127():
+    return Mod(12, 7)
+
     
-def test_mod_creation(create_test_mod):
-    assert create_test_mod.value == 2
+def test_mod_creation(mod_53):
+    assert mod_53.value == 2
 
 def test_negative_modulus():
     with pytest.raises(ValueError):
@@ -45,19 +45,19 @@ def test_non_int_modulus():
 def test_multiple_mods(create_test_mods):
     assert create_test_mods.value % create_test_mods.modulus == create_test_mods.value
 
-def test_residue_value(create_test_mod):
-    assert create_test_mod.value == 2
+def test_residue_value(mod_53):
+    assert mod_53.value == 2
 
-def test_modulus_value(create_test_mod):
-    assert create_test_mod.modulus == 3
+def test_modulus_value(mod_53):
+    assert mod_53.modulus == 3
 
-def test_read_only_value(create_test_mod):
+def test_read_only_value(mod_53):
     with pytest.raises(AttributeError):
-        create_test_mod.value = 3
+        mod_53.value = 3
 
-def test_read_only_modulus(create_test_mod):
+def test_read_only_modulus(mod_53):
     with pytest.raises(AttributeError):
-        create_test_mod.modulus = 6
+        mod_53.modulus = 6
 
 def test_equal_2_mods(mod_02, mod_22):
     assert mod_02 == mod_22
@@ -82,3 +82,81 @@ def test_equal_mod_equal_hash(mod_02, mod_22):
 
 def test_unequal_mod_unequal_hash(mod_22, mod_53):
     assert hash(mod_22) != hash(mod_53)
+
+def test_int_from_mod(mod_22, mod_53):
+    assert (int(mod_22), int(mod_53)) == (0, 2)
+
+def test_mod_str(mod_53):
+    assert str(mod_53) == 'Mod(value=2, modulus=3)'
+
+def test_mod_repr(mod_22):
+    assert repr(mod_22) == 'Mod(value=0, modulus=2)'
+
+@pytest.mark.parametrize('test_value, expected', [
+                                                  (Mod(5, 3), ValueError),
+                                                  (Mod(7, 2), 1),
+                                                  (Mod(16, 5), ValueError),
+                                                  (5, 1),
+                                                  (2, 0)
+                                                  ])
+def test_mod_add(mod_02, test_value, expected):
+    if type(expected) == type and issubclass(expected, Exception):
+        with pytest.raises(ValueError):
+            mod_02 + test_value
+    else:
+        assert mod_02 + test_value == expected
+
+@pytest.mark.parametrize('test_value, expected', [
+                                                  (Mod(5, 3), ValueError),
+                                                  (Mod(7, 2), 1),
+                                                  (Mod(16, 5), ValueError),
+                                                  (5, 1),
+                                                  (2, 0)
+                                                  ])
+def test_mod_sub(mod_02, test_value, expected):
+    if type(expected) == type and issubclass(expected, Exception):
+        with pytest.raises(ValueError):
+            mod_02 - test_value
+    else:
+        assert mod_02 - test_value == expected
+
+@pytest.mark.parametrize('test_value, mod_22_result, mod_127_result', [
+                                                  (Mod(5, 3), ValueError, ValueError),
+                                                  (Mod(7, 2), 0, ValueError),
+                                                  (Mod(16, 5), ValueError, ValueError),
+                                                  (5, 0, 4),
+                                                  (2, 0, 3),
+                                                  (Mod(3, 7), ValueError, 1)
+                                                  ])
+def test_mod_mul(mod_22, mod_127, test_value, mod_22_result, mod_127_result):
+    if type(mod_22_result) == type and issubclass(mod_22_result, Exception):
+        with pytest.raises(ValueError):
+            mod_22 * test_value
+    else:
+        assert mod_22 * test_value == mod_22_result
+    if type(mod_127_result) == type and issubclass(mod_127_result, Exception):
+        with pytest.raises(ValueError):
+            mod_127 * test_value
+    else:
+        assert mod_127 * test_value == mod_127_result
+
+@pytest.mark.parametrize('test_value, mod_22_result, mod_127_result', [
+                                                  (Mod(5, 3), ValueError, ValueError),
+                                                  (Mod(7, 2), 0, ValueError),
+                                                  (Mod(16, 5), ValueError, ValueError),
+                                                  (5, 0, 3),
+                                                  (2, 1, 4),
+                                                  (0, 1, 1),
+                                                  (Mod(3, 7), ValueError, 6)
+                                                  ])
+def test_mod_pow(mod_22, mod_127, test_value, mod_22_result, mod_127_result):
+    if type(mod_22_result) == type and issubclass(mod_22_result, Exception):
+        with pytest.raises(ValueError):
+            mod_22 ** test_value
+    else:
+        assert mod_22 ** test_value == mod_22_result
+    if type(mod_127_result) == type and issubclass(mod_127_result, Exception):
+        with pytest.raises(ValueError):
+            mod_127 ** test_value
+    else:
+        assert mod_127 ** test_value == mod_127_result
